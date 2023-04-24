44d96e7e9cc25434faf59401335d6e0ae133717f
PTaank04 <107695048+PTaank04@users.noreply.github.com>
2023-04-24 21:53:43 +0100

    update app.js

--- app.js
+++ app.js
@@ -23,32 +23,38 @@ const server = http.createServer(app);
 app.use(express.json());
 app.use(cors("*"))
 
-app.get('/id',async(req,res)=>{
-    console.log("req-->",req.hostname)
-    console.log("ip",req.query.ip)
+app.get('/id', async (req, res) => {
+    console.log("req-->", req.hostname)
+    console.log("ip", req.query.ip)
     const ip = req.query.ip;
     const cmd = "nmap -Pn " + ip
     let output = ''
-    const response = exec(cmd,(error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-            output = stdout
-        })
-    
-        response.on('close', (code) => {
-            
-            console.log('process has exited',output)
+    const response = exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    })
+
+    response.on('close', (code) => {
+
+        console.log('process has exited', output)
+        if (output) {
             return res.status(200).send({
                 output
             })
-        })
+        } else {
+            return res.status(302).send({
+                error: "Not found or invalid IP"
+            })
+        }
+
+    })
 
 
     // return res.status(200).send({
@@ -56,13 +62,13 @@ app.get('/id',async(req,res)=>{
     // })
 })
 
-app.post('/check-port',(req,res)=>{
+app.post('/check-port', (req, res) => {
     console.log("check Port", req.body)
     const ip = req.body.text;
     const cmd = "nmap -Pn " + ip
     try {
         let output = ""
-        const response = exec(cmd,(error, stdout, stderr) => {
+        const response = exec(cmd, (error, stdout, stderr) => {
             if (error) {
                 console.log(`error: ${error.message}`);
                 return;
@@ -74,35 +80,35 @@ app.post('/check-port',(req,res)=>{
             console.log(`stdout: ${stdout}`);
             output = stdout
         })
-    
+
         response.on('close', (code) => {
-            
-            console.log('process has exited',output)
-            if(output){
+
+            console.log('process has exited', output)
+            if (output) {
                 return res.status(200).send({
                     output
                 })
-            }else{
+            } else {
                 return res.status(302).send({
                     error: "Not found or invalid IP"
                 })
             }
-            
+
         })
-        
+
     } catch (error) {
         res.status(400).send({
             error: "some error ocuurs"
         })
     }
-    
+
 
 
 })
 
 const PORT = 8080
 
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log(`server is running on the ${PORT}`)
 })
 
